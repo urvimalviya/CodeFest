58bf93886e295691e27b71c5a5411aea6e45e8f2
urvimalviya <urvimalviya@gmail.com>
2017-04-07 02:46:47 -0400

    CodeFest

--- OrigApp/ViewPager/app/src/main/java/com/journaldev/viewpager/MainActivity.java
+++ OrigApp/ViewPager/app/src/main/java/com/journaldev/viewpager/MainActivity.java
@@ -1,10 +1,14 @@
 package com.journaldev.viewpager;
 
-import android.support.v4.view.ViewPager;
+import android.content.Intent;
+import android.support.v4.view.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +20,19 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new CustomPagerAdapter(this));
 
-    }
+        
 
+        Button trainingBtn = (Button) findViewById(R.id.trainingBtn);
+        Button nutritionBtn = (Button) findViewById(R.id.nutritionBtn);
+
+
+        nutritionBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activityChangeIntent = new Intent(MainActivity.this, NutritionActivity.class);
+                MainActivity.this.startActivity(activityChangeIntent);
+            }
+        }
+        );
+    }
 
 }
